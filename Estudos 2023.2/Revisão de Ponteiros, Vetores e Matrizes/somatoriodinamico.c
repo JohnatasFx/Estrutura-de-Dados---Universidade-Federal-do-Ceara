9962226db6d7a30b2b23543d745a203394bd48b2
Johnatas Félix <johnatasfelix04@gmail.com>
2023-08-15 01:36:18 -0300

    adicionando novos exemplos

---
+++ Estudos 2023.2/Revisão de Ponteiros, Vetores e Matrizes/somatoriodinamico.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float somatorio(int n, float *v) {
+
+	int i;
+	float s = 0.0F;
+	for(i = 0; i <n; i++) {
+		s += v[i];
+	}
+
+	return s;
+}
+
+int main() {
+
+	float *v;
+	float s;
+	int n;
+	int i;
+
+	scanf("%d", &n);
+	getchar();
+
+	v = (float*)malloc(n*sizeof(float));
+	if(v) {
+		for(i=0; i<n; i++) {
+			scanf("%f", &v[i]);
+		}
+
+		s = somatorio(n,v);
+		printf("%f", s);
+
+		free(v);
+	}
+
+	return 0;
+}
+
